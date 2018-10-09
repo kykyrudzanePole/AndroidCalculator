1fdeb1e1cc56713fd5a5a7ae67e6bcc383aad927
Zakhar <zakharkostyshyn@gmail.com>
2018-10-09 09:46:10 +0300

    help me plz

--- Android/app/src/main/java/com/example/couple/android/MainActivity.java
+++ Android/app/src/main/java/com/example/couple/android/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.couple.android;
 
-import android.annotation.SuppressLint;
-import android.os.PatternMatcher;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,7 +8,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
